35a0dc01ad28502206c514682947b3bf5226e94c
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2021-06-24 01:40:42 +0200

    theme...

--- src/js/foodservice.js
+++ src/js/foodservice.js
@@ -5,12 +5,32 @@ import markupTpl from '../templates/markap.hbs';
 
 const menuContainerRef = document.querySelector(".js-menu");
 // console.log(menuContainerRef);
-
 const MenuCardsMarkup = createMenuCardsMarkup(menu);
 // console.log(MenuCardsMarkup);
-
 menuContainerRef.insertAdjacentHTML('beforeend', MenuCardsMarkup);
 
 function createMenuCardsMarkup(menu) {
     return menu.map(markupTpl).join('');
+}
+
+const bodyRef = document.querySelector('body');
+
+const themeSwitchToggle = document.querySelector("#theme-switch-toggle");
+console.dir(themeSwitchToggle);
+
+themeSwitchToggle.addEventListener('change', onThemeSwitchToggleKlick);
+
+ const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
+    
+function onThemeSwitchToggleKlick(evt) {
+   
+    bodyRef.classList.toggle(Theme.DARK);
+    // if (bodyRef.classList.contains(Theme.DARK)) {
+    //     bodyRef.classList.toggle(Theme.LIGHT)
+    // }
+
+    
 }
